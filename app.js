3cf07faa8853c732b3a9595294643a08b9345a35
nwebb123 <68877910+nwebb123@users.noreply.github.com>
2022-10-04 21:58:55 -0400

    MVP complete!
    MVP complete, last steps include organizing functionality into their own modules and importing them into one js file.

--- app.js
+++ app.js
@@ -11,20 +11,8 @@ class Book {
 class UI {
     static displayBooks() {
         //Hard-coded array of books; sample data before local storage is implemented
-        const StoredBooks = [{
-                title: 'Slaughterhouse Five',
-                author: 'Kurt Vonnegut',
-                isbn: '189459'
-            },
-            {
-                title: 'To Kill A Mockingbird',
-                author: 'Harper Lee',
-                isbn: '214896'
-            },
 
-        ];
-
-        const books = StoredBooks;
+        const books = Store.getBooks();
 
         books.forEach((book) => UI.addBookToList(book));
     }
@@ -55,7 +43,7 @@ class UI {
 
         alertDiv.className = `alert alert-${className}`;
         alertDiv.appendChild(document.createTextNode(message));
-        
+
         const container = document.querySelector('.container');
         const form = document.querySelector('.form-row');
         //insertBefore is placing the newly created alertDiv directly above row containing the form. 
@@ -72,7 +60,44 @@ class UI {
 }
 
 //Store Class: Handles storage
+class Store {
+    static getBooks() {
+        let books;
 
+        //is there a current book in book storage?
+        if (localStorage.getItem('books') === null) {
+            books = []
+        } else {
+            //Need to use JSON.parse b/c local storage stores everything as a string. In short, turn string into object.
+            books = JSON.parse(localStorage.getItem('books'));
+        }
+
+        return books;
+    }
+
+    static addBook(book) {
+        const books = Store.getBooks();
+
+        books.push(book);
+        //Same as above, except now we're turning books object into string in order to store it into localStorage.
+        localStorage.setItem('books', JSON.stringify(books));
+    }
+
+    //isbn is used like a primary key since it is unique (or is supposed to be...possible validation feature). Matches isbn of (X) element with isbn of book stored in localStorage 
+    static removeBook(isbn) {
+
+        const books = Store.getBooks();
+
+        //Loop through each book/index and if a books isbn matches the isbn passed in, it will slice it from the array books.
+        books.forEach((book, index) => {
+            if (book.isbn === isbn) {
+                books.splice(index, 1)
+            }
+        });
+
+        localStorage.setItem('books', JSON.stringify(books));
+    }
+}
 
 //Event to display books
 document.addEventListener('DOMContentLoaded', UI.displayBooks)
@@ -87,7 +112,6 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
     const author = document.querySelector('#author').value;
     const isbn = document.querySelector('#isbn').value;
 
-
     //Validate Form
     if (title === '' || author === '' || isbn === '') {
         UI.showAlert('Please fill in all fields', 'danger');
@@ -99,14 +123,14 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
         //Add Book to UI
         UI.addBookToList(book);
 
+        //Add book to local storage
+        Store.addBook(book);
+
         //Show Success Alert Message
         UI.showAlert('Book Added', 'success');
 
-
         UI.clearFields();
     }
-
-
 })
 
 //Event to remove a book
@@ -114,7 +138,10 @@ document.querySelector('#book-list').addEventListener('click', (e) => {
     // e.target is passed in to select the X for a particular book. Can't use id of X, but instead we can target the parent and add an event to pass in the particular book.
     UI.deleteBook(e.target);
 
+    //Remove book from local storage; Traverse the DOM using previousElementSibling.
+    Store.removeBook(e.target.parentElement.previousElementSibling.textContent)
 
     //Success Alert message
     UI.showAlert('Book removed', 'success');
 })
+
